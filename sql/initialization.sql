344ed0d6a7b9faac5af5c7ded9a5306fef477c51
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-01-08 22:23:53 -0500

    Add SQL scripts

---
+++ sql/initialization.sql
@@ -0,0 +1,9 @@
+USE mysql;
+
+-- Creación de los esquemas
+DROP SCHEMA IF EXISTS tracklink;
+CREATE SCHEMA tracklink;
+DROP SCHEMA IF EXISTS dbstg;
+CREATE SCHEMA dbstg;
+DROP SCHEMA IF EXISTS dbsor;
+CREATE SCHEMA dbsor;
